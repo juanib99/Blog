cd2556d6a8a8fc533f6f7c2eb9d849c8673094c3
Juan Francisco Calo <juanfcalo97@gmail.com>
2022-06-30 21:19:34 -0300

    Form con CKeditor

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Blog(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     titulo = models.CharField(max_length=100, verbose_name='Titulo')
     subtitulo = models.CharField(max_length=100, verbose_name='Subtitulo')
-    texto = models.TextField()
+    texto = RichTextField(blank=True, null=True)
     autor = models.CharField(max_length=40, verbose_name='Autor')
     fecha = models.DateTimeField()
     imagen = models.ImageField(upload_to='imagenes/')
